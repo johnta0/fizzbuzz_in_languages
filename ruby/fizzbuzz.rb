a1987aa8f12fe12695e722b8cd0f477279c519a4
him0 <hiroki-nakashima@c-fo.com>
2018-03-09 00:57:22 +0900

    Update ruby script

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -1,18 +1,10 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*- 
 
-def standard_fizzbuzz
-  (1..100).each do |num|
-    if num % 3 == 0 && num % 5 == 0
-      puts "fizzbuzz"
-    elsif num % 3 == 0
-      puts "fizz"
-    elsif num % 5 == 0
-      puts "buzz"
-    else
-      puts num
-    end
-  end
+(1..100).each do |num|
+  out = ''
+  out << 'fizz' if num % 3 == 0
+  out << 'buzz' if num % 5 == 0
+  out = num if out == ''
+  puts(out)
 end
-
-standard_fizzbuzz
