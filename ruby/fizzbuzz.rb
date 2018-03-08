b51b28c0acad377bbb98125ba28f8e4e3ae3e178
him0 <hiroki-nakashima@c-fo.com>
2018-03-09 01:37:33 +0900

    :heart: restore deleted codes

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -1,10 +1,29 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*- 
 
-(1..100).each do |num|
-  out = ''
-  out << 'fizz' if num % 3 == 0
-  out << 'buzz' if num % 5 == 0
-  out = num if out == ''
-  puts(out)
+def standard_fizzbuzz
+  (1..100).each do |num|
+    if num % 3 == 0 && num % 5 == 0
+      puts "fizzbuzz"
+    elsif num % 3 == 0
+      puts "fizz"
+    elsif num % 5 == 0
+      puts "buzz"
+    else
+      puts num
+    end
+  end
 end
+
+def shorter_fizzbuzz
+  (1..100).each do |num|
+    out = ''
+    out << 'fizz' if num % 3 == 0
+    out << 'buzz' if num % 5 == 0
+    out = num if out == ''
+    puts(out)
+  end
+end
+
+standard_fizzbuzz
+shorter_fizzbuzz
